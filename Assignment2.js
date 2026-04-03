d1426561dd56fd9f178efb2ce965edc2713f5ffd
emilygaber344-rgb <emilygaber344@gmail.com>
2026-04-03 11:58:37 -0500

    Update Assignment2.js

--- Assignment2.js
+++ Assignment2.js
@@ -11,5 +11,24 @@ function validateFName() {
   if (x == "") {
     alert("Name must be filled out");
     return false;
+    
+   // credit to geeksforgeeks and you professor
+    
+        const d = new Date();
+        let text = d.toLocaleDateString();
+        document.getElementById("today").innerHTML = text;
+
+          const dp = document.getElementById('date');
+        dp.setAttribute('min', '1909-01-01');
+        dp.setAttribute('max', 'today');
+        const minDate = dp.getAttribute('min');
+        const maxDate = dp.getAttribute('max');
+        document.getElementById('dateRange').textContent = 
+            'Accepted Date Range: ' + minDate + ' to ' + maxDate;
+        dp.addEventListener('change', function () {
+            const dateSelect = dp.value;
+            document.getElementById('selectedDate').textContent = 
+                'Selected Date: ' + dateSelect;
+        });
   }
 }
