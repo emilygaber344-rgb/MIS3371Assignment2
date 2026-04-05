b396e95e408da11d0e6b7b04a21a4032f52b1fe6
emilygaber344-rgb <emilygaber344@gmail.com>
2026-04-04 22:49:51 -0500

    Update Assignment2.js

--- Assignment2.js
+++ Assignment2.js
@@ -7,7 +7,10 @@ Purpose: Learning html
 */
 
 function validateFName() {
-  alert("Please add a valid First Name.");
+ let x = document.forms["form"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
 //credit w3schools tyty
 function clear() {
   document.getElementById("Intake").reset();
