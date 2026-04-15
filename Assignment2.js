b21dbb6faec3289631f0fedc61da5df412108839
emilygaber344-rgb <emilygaber344@gmail.com>
2026-04-15 12:27:20 -0500

    Update Assignment2.js

--- Assignment2.js
+++ Assignment2.js
@@ -14,34 +14,7 @@ function validateFName() {
 
  
 /*credit to w3schools
-	  function passwordreq(){
-		  var myInput = document.getElementById("password");
-var letter = document.getElementById("letter");
-var capital = document.getElementById("capital");
-var number = document.getElementById("number");
-var length = document.getElementById("length");
-
-// When the user clicks on the password field, show the message box
-myInput.onfocus = function() {
-  document.getElementById("message").style.display = "block";
-}
-
-// When the user clicks outside of the password field, hide the message box
-myInput.onblur = function() {
-  document.getElementById("message").style.display = "none";
-}
-
-// When the user starts to type something inside the password field
-myInput.onkeyup = function() {
-  // Validate lowercase letters
-  var lowerCaseLetters = /[a-z]/g;
-  if(myInput.value.match(lowerCaseLetters)) {
-    letter.classList.remove("invalid");
-    letter.classList.add("valid");
-  } else {
-    letter.classList.remove("valid");
-    letter.classList.add("invalid");
-}
+	 
 */
   // Validate capital letters
   var upperCaseLetters = /[A-Z]/g;
